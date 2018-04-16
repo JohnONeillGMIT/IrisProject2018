858dec3f1c2a1365aed2d431003eeea74edee655
O'Neill <John_J_O'Neill@dell.com>
2018-04-17 00:27:16 +0100

    John O'Neill 16/04/18 - Pulling in csv file for inital Numpy views

---
+++ Numpy.py
@@ -0,0 +1,16 @@
+# John O'Neill 16/04/2018 Inital Working with Numpy
+# Taken from Ians Lecture on Mathplotlib https://web.microsoftstream.com/video/f0788c1c-c7bd-4347-98ac-477186938ed7
+
+
+#Calculating the mean of Col 1
+
+import numpy
+
+#read in the Iris file
+
+data = numpy.genfromtxt('Iris.csv', delimiter= ',')
+
+firstcol = data[:,0]
+meanfirstcol =numpy.mean(data[:0])
+
+
