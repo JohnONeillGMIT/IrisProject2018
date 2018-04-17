e708a1de4359693c946c29085abaa1c1b3ce6483
O'Neill <John_J_O'Neill@dell.com>
2018-04-17 21:35:39 +0100

    code added to print Min,Max,Mean of data set (Column1)

--- Numpy.py
+++ Numpy.py
@@ -8,10 +8,14 @@ import numpy
 
 #read in the Iris file
 
-data = numpy.genfromtxt('Iris.csv', delimiter= ',',dtype=float)
+data = numpy.genfromtxt('iris.csv', dtype=float, delimiter= ',') #pulls in the iris.csv file, delimited by ','
 
-#firstcol = data[:,0]
-#meanfirstcol =numpy.nanmean(data[:,0])
+firstcol = data[:,0] #assigns the first column of data from the csv file
+meanfirstcol =numpy.nanmean(data[:,0]) #added nan so that columns with no value #n/a wont drive an error
+minfirstcol =numpy.nanmin(data[:,0])
+maxfirstcol =numpy.nanmax(data[:,0])
 
-
+print("Average of the First column is:", meanfirstcol)
+print("Min of the First column is:", minfirstcol)
+print("Max of the First column is:", maxfirstcol)
 
